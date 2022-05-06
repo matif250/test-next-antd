2c3b2d3d9107797a5396700f71b9e0979674fd18
Muhammad Atif <matif250>
2022-05-06 16:26:54 +0500

    install sass

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import '../styles/globals.css'
-
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import 'styles/globals.scss'
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
